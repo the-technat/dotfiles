36f9bc666f715906355ef513422c570b1c0fc389
Nathanael Liechti <technat@technat.ch>
2022-08-08 21:47:03 +0200

    feat: add option for screen recording without keys

--- sway/.config/sway/end-record.sh
+++ sway/.config/sway/end-record.sh
@@ -12,12 +12,10 @@
 pidWshowkeys=$(cat /tmp/wshowkeys.pid)
 pidWFrecorder=$(cat /tmp/wfrecorder.pid)
 
-if [ $pidWFrecorder != "" ] && [ $pidWshowkeys != "" ]
+if [ $pidWFrecorder != "" ] 
 then
-  kill -SIGTERM $pidWshowkeys 
   kill -INT $pidWFrecorder 
   notify-send -t 5000 "Recording has been saved to ~/Videos/record$(date +%d%m%y)-$(date +%H%M%S).mp4"
-  rm /tmp/wshowkeys.pid
   rm /tmp/wfrecorder.pid
   exit 0
 else
@@ -25,5 +23,10 @@ else
   exit 1
 fi
 
-
+if [ $pidWshowkeys != "" ]
+then
+  kill -SIGTERM $pidWshowkeys 
+  rm /tmp/wshowkeys.pid
+fi
+  
 
--- sway/.config/sway/start-record.sh
+++ sway/.config/sway/start-record.sh
@@ -21,8 +21,13 @@ then
 fi
 
 ## Start wshowkeys
-wshowkeys -F 'Rubik 30' -b '#073642' -f '#839496' -s '#859900' -a bottom -m 10 -t 2 &
-pidWshowkeys=$(echo $!)
+# if --no-keys is passed in, skip wshowkeys
+if [ $1 != "--no-keys" ]
+then
+  wshowkeys -F 'Rubik 30' -b '#073642' -f '#839496' -s '#859900' -a bottom -m 10 -t 2 &
+  pidWshowkeys=$(echo $!)
+  echo $pidWshowkeys > /tmp/wshowkeys.pid
+fi
 
 ## Find default audio source
 defaultSource=$(pactl get-default-source)
@@ -31,11 +36,7 @@ defaultSource=$(pactl get-default-source)
 currentFocusedMonitor=$(swaymsg -t get_outputs --raw | jq '. | map(select(.focused == true)) | .[0].name' -r)
 wf-recorder -a $defaultSource -o $currentFocusedMonitor -f ~/Videos/record$(date +%d%m%y)-$(date +%H%M%S).mp4 &
 pidWFrecorder=$(echo $!)
-
-notify-send -t 5000 "Started recording on $currentFocusedMonitor using Audio from $defaultSource"
-
-## Save both pids in file
-echo $pidWshowkeys > /tmp/wshowkeys.pid
 echo $pidWFrecorder > /tmp/wfrecorder.pid
+notify-send -t 5000 "Started recording on $currentFocusedMonitor using Audio from $defaultSource"
 
 exit 0
