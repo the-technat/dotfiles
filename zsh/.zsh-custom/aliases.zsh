ba77a42924b72d1b225e69bb6ea6481e5188309b
Nathanael Liechti <technat@technat.ch>
2022-09-01 16:13:51 +0200

    feat: add function to spin up local argocd env

--- zsh/.zsh-custom/aliases.zsh
+++ zsh/.zsh-custom/aliases.zsh
@@ -35,6 +35,41 @@ k3ddd () {
   k3d cluster create $name -a 2 --api-port $IP:6550 -p 443:443@loadbalancer -p 80:80@loadbalancer
 }
 
+localArgoEnv() {
+  inGitRepo=$(git status)
+  if [ $? -eq 0 ]
+  then
+    kubectl create ns argocd
+    kubectl apply -n argocd --force -f manifests/install.yaml
+    kubectl patch -n argocd cm argocd-cmd-params-cm --patch '{"data": {"server.insecure": "true"}}'
+    kubectl rollout -n argocd restart deployment argocd-server
+    cat <<EOF | kubectl apply -f -
+      apiVersion: networking.k8s.io/v1
+      kind: Ingress
+      metadata:
+        name: argocd-server
+        namespace: argocd
+        annotations:
+          ingress.kubernetes.io/ssl-redirect: "true"
+      spec:
+        rules:
+        - host: localhost
+          http:
+            paths:
+            - path: /
+              pathType: Prefix
+              backend:
+                service:
+                  name: argocd-server
+                  port:
+                    number: 80
+EOF
+    kubectl wait --for=jsonpath='{.kind}'=Secret secret/argocd-initial-admin-secret
+    pw=$(kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath='{.data.password}' |base64 -d)
+    echo "Argo CD UI at https://localhost using User admin and password $pw"
+  fi
+}
+
 ##################
 # PGP 
 ##################
