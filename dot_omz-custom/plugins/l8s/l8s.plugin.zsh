226ed191e0e568f02b6f1d12ae9db5288f9f1c0c
Nathanael Liechti <technat@technat.ch>
2023-10-16 13:09:54 +0000

    feat: refacotr l8s toolchaain

--- dot_omz-custom/plugins/l8s/l8s.plugin.zsh
+++ dot_omz-custom/plugins/l8s/l8s.plugin.zsh
@@ -77,6 +77,11 @@ EOF
 
 deleteCluster() {
   checkClusterName $1
+
+  kubectl delete ingress --all-namespaces --all
+  sleep 5
+  kubectl delete namespace tailscale
+  sleep 5
   k3d cluster delete $1
   docker network rm $1
   sudo hostess rm $1.local 
@@ -119,10 +124,10 @@ EOF
     rm $config
 
     # finally set one DNS record in your hosts file for the entire cluster
-    sudo hostess add $1.local $gateway
+    sudo hostess add $1.local $gateway 
 
     # k3d is k3s which is k8s with batteries, but these batteries sometimes need tuning
-    docker cp $HOME/.omz-custom/plugins/l8s/patches/traefik-patch.yaml k3d-$1-server-0:/var/lib/rancher/k3s/server/manifests/
+    docker cp $HOME/.omz-custom/plugins/l8s/patches/traefik-patch.yaml k3d-$1-server-0:/var/lib/rancher/k3s/server/manifests/ > /dev/null
 
     echo "Created cluster $1 with control-plane available at $gateway:6443 and traefik (ingress-controller) at $gateway:443"
 }
@@ -135,39 +140,49 @@ toolChain() {
   # Installation of tools must be reproducable, so that a rerun just reconfigures the tool
   case "$1" in          
     (tailscale)
-      echo "Installing Tailsacle to namespace tailscale"
+      echo "Installing Tailscale to namespace tailscale"
       kubectl create namespace tailscale --dry-run=client -o yaml | kubectl apply -f - > /dev/null # idempotent
       echo "Ensure you have met the prerequisites described in https://tailscale.com/kb/1236/kubernetes-operator/#setting-up-the-kubernetes-operator"
       echo "Please enter the clientID of your OAuth app:"
       read clientID
       echo "Please enter the clientSecret of your OAuth app:"
       read clientSecret
-      cat <<EOF | tpl -f $HOME/.omz-custom/plugins/l8s/templates/tailscale-operator-template.yaml > /tmp/tailscale.yaml
+      kubectl delete secret -n tailscale operator-oauth > /dev/null
+      kubectl create secret generic operator-oauth --from-literal client_id=$clientID --from-literal client_secret=$clientSecret -n tailscale > /dev/null
+      cat <<EOF | tpl -f $HOME/.omz-custom/plugins/l8s/templates/tailscale-operator-template.yaml > $(pwd)/tailscale.yaml
       {
-        "client_id": "$clientID",
-        "client_secret": "$clientSecret"
+        "name": "$name"
       }
 EOF
-      kubectl apply -f /tmp/tailscale.yaml -n tailscale > /dev/null
-      rm /tmp/tailscale.yaml
+      kubectl apply -f $(pwd)/tailscale.yaml > /dev/null
       echo "Waiting for tailscale operator to become available..."
       sleep 2
       pod=$(kubectl get pods -l "app=operator" -n tailscale -o=jsonpath='{.items[0].metadata.name}')
       kubectl wait -n tailscale --timeout=600s --for=condition=Ready pod/$pod > /dev/null 
+      dnsSuffix=$(tailscale status -json | jq '.MagicDNSSuffix' | tr -d "\"")
+      echo "Successfully installed Tailscale onto your cluster"
+      echo "Traefik is now reachable on https://$name.$dnsSuffix, use subPaths to expose services"
       ;;
     (argocd)
       echo "Installing Argo CD to namespace argocd"
-        ## Argo CD
       kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f - > /dev/null # idempotent
       containsRepo=$(helm repo list |grep argo)
       if [[ $containsRepo == "" ]]
       then
           helm repo add argo https://argoproj.github.io/argo-helm
       fi
+      echo "Do you want Argo CD to be exposed using tailscale or traefik?"
+      read expose
+      if [[ $expose == "tailscale" ]]; then
+        dnsSuffix=$(tailscale status -json | jq '.MagicDNSSuffix' | tr -d "\"")
+      else
+        dnsSuffix="local"
+      fi
       cat <<EOF | tpl -f $HOME/.omz-custom/plugins/l8s/templates/argocd-template.yaml > $(pwd)/argocd.yaml
       {
-        "argocd_url": "https://$name.local/argocd",
-        "dex_url": "https://$name.local/dex"
+          "expose": "$expose",
+          "name": "$name",
+          "dnsSuffix": "$dnsSuffix"
       }
 EOF
       echo "Argo CD values have been saved into your current directroy"
@@ -178,11 +193,11 @@ EOF
       kubectl wait -n argocd --timeout=600s --for=condition=Ready pod/$pod > /dev/null 
       kubectl wait -n argocd --for=jsonpath='{.kind}'=Secret secret/argocd-initial-admin-secret > /dev/null
       sleep 2
-      echo "Argo CD UI at https://$name.local/argocd using user admin and password $(kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath='{.data.password}' |base64 -d)"
+      echo "Argo CD UI at https://$name.$dnsSuffix/argocd using user admin and password $(getArgoAdminPW)"
+      echo "Make sure you expose Dex the same way if you want to do \"Login With Dex\""
       echo "Update Argo CD using: helm upgrade -i argocd -n argocd argo/argo-cd -f ./argocd.yaml"
       ;;
     (dex)
-      ## Dex
       echo "Installing Dex to namespace dex"
       kubectl create namespace dex --dry-run=client -o yaml | kubectl apply -f - > /dev/null # idempotent 
       containsRepo=$(helm repo list |grep dex)
@@ -190,74 +205,37 @@ EOF
       then
         helm repo add dex https://charts.dexidp.io
       fi
-      echo "Please enter a Github Organization to use for Login With Github:"
+      echo "Do you want Dex to be exposed using tailscale or traefik?"
+      read expose
+      if [[ $expose == "tailscale" ]]; then
+        dnsSuffix=$(tailscale status -json | jq '.MagicDNSSuffix' | tr -d "\"")
+      else
+        dnsSuffix="local"
+      fi
+      echo "Please enter a Github Organization to use for \"Login With Dex\":"
       read org
       echo "Now head over to https://github.com/organizations/$org/settings/applications and create a new OAuth app"
       echo "Please enter the clientID of the OAuth app:"
       read clientID
       echo "Please enter the clientSecret of the OAuth app:"
       read clientSecret
-      cat <<EOF | tpl -f $HOME/.omz-custom/plugins/l8s/templates/dex-template.yaml > /tmp/dex.yaml
+      kubectl delete secret -n dex github-oauth-creds > /dev/null
+      kubectl create secret generic github-oauth-creds --from-literal client_id=$clientID --from-literal client_secret=$clientSecret -n dex > /dev/null
+      cat <<EOF | tpl -f $HOME/.omz-custom/plugins/l8s/templates/dex-template.yaml > $(pwd)/dex.yaml
     {
-      "client_id": "$clientID",
-      "client_secret": "$clientSecret",
       "org": "$org",
-      "dexRedirectURI": "https://$name.local/dex/callback",
-      "argoRedirectURI": "https://$name.local/argocd/auth/callback",
-      "issuer": "https://$name.local/dex"
+      "dnsSuffix": "$dnsSuffix",
+      "name": "$name"
     }
 EOF
-      helm upgrade -i -n dex dex dex/dex -f /tmp/dex.yaml > /dev/null
-      if [[ $? -eq 0 ]]; then
-          rm -rf /tmp/dex.yaml
-          echo "Dex successfully installed, please make sure you set the redirectURI in the OAuth App to https://$name.local/dex/callback"
-      else
-          echo "Dex installation failed, see /tmp/dex.yaml for values"
-      fi
+      echo "Dex values have been saved into your current directroy"
+      helm upgrade -i -n dex dex dex/dex -f $(pwd)/dex.yaml > /dev/null
+      echo "Dex successfully installed, please make sure you set the redirectURI in the OAuth App to https://$name.$dnsSuffix/dex/callback"
       ;;
     (*)
         ;;
     esac
 
-}
-
-enableTailscaleFunnel() {
-    checkClusterName $1
-    checkClusterExists $1
-  
-    tailscalePath=$(which tailscale)
-    if [[ ! -f "$tailscalePath" ]]; then
-        echo "Please install tailscale for this feature"
-        echo "https://tailscale.com/kb/installation/"
-        return
-    fi
-
-    tailscaleStatus=$(tailscale status)
-    if [[ "$tailscaleStatus" == "Logged out." ]]; then
-        echo "Tailscale must be up & running"
-        echo "Use sudo tailscale up --ssh to activate"
-        return
-    fi
-
-    # the funnel can only be active for one cluster at the tiem
-    funnelStatus=$(tailscale funnel status)
-    if [[ "$funnelStatus" == "No serve config" ]]; then
-        sudo tailscale serve https $2 127.0.0.1:$3
-        sudo tailscale funnel 443 on
-        tailscale funnel status
-    else 
-        listOfActivePaths=$(tailscale funnel status --json | jq '.Web | to_entries[] | select(.key|startswith("")) | .value.Handlers | to_entries[] | .key' | tr -d "\"" | tr '\n' ' '  )
-        if [[ ! ${listOfActivePaths[@]} =~ $2 ]]; then
-            sudo tailscale serve https $2 127.0.0.1:$3
-            echo "Funnel already active, added new subPath to the serve config"
-            tailscale funnel status
-        else
-            echo "Funnel is currently active and the specified path is already in use"
-            echo "Please active the funnel yourself"
-            echo "Example: sudo tailscale serve https $2 127.0.0.1:$3"
-        fi
-    fi
-    
 }
 
 #### Helper functions
@@ -274,6 +252,10 @@ checkClusterExists() {
       echo "Cluster doesn't exist"
       exit 0
   fi
+}
+
+getArgoAdminPW() {
+  kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath='{.data.password}' |base64 -d
 }
 
 # https://unix.stackexchange.com/questions/55913/whats-the-easiest-way-to-find-an-unused-local-port
