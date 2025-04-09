c0c60be1654c01382e6968b122498b545ef1e974
Nathanael Liechti <technat@technat.ch>
2025-04-09 16:27:57 +0200

    feat: add git to prerequisites

--- ensure-deps.sh
+++ ensure-deps.sh
@@ -2,6 +2,37 @@
 # https://www.chezmoi.io/reference/configuration-file/hooks/
 # this script runs every time the source state is read, so the faster is exits, the less you have to wait
 
+## git
+if ! command -v "git" > /dev/null; then
+  case "$(uname -s)" in
+    Darwin)
+      xcode-select --install
+      ;;
+    Linux)
+      . /etc/os-release
+      case $ID in
+        debian|ubuntu|mint)
+          export DEBIAN_FRONTEND=noninteractive
+          sudo apt -qq -y install git < /dev/null > /dev/null
+          ;;
+        rocky|fedora|rhel|centos)
+          sudo dnf -y install git 
+          ;;
+        archarm|arch)
+          sudo pacman -S --noconfirm git 
+          ;;
+        *)
+          echo -n "unsupported linux distro, install git manually"
+          ;;
+      esac
+    ;;
+    *)
+      echo "Unknown OS, install git manually"
+      exit 1
+      ;;
+  esac
+fi
+
 ## zsh
 if ! command -v "zsh" > /dev/null; then
   case "$(uname -s)" in
