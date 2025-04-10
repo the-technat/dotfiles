af8aee214f27000edef7eb510cbdd4394bdeacd1
Nathanael Liechti <technat@technat.ch>
2025-04-10 14:58:43 +0200

    feat: introduze zunit

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+if [[ -z ${ZSH_SOURCED} ]]; then
+  source ~/.zshrc
+fi
+
+if [ "${TERM:-}" = "" ]; then
+  echo "Setting TERM to dumb" # makes tput happy in CI envs
+  export TERM="dumb"
+fi
+
+# Run the tests
+zunit run
