b19af6aa65b89740eaa64becb2355796c17f5ad6
Nathanael Liechti <technat@technat.ch>
2025-04-15 09:22:03 +0200

    feat: also install system packages in the beginning

--- home/ensure-deps.sh
+++ home/ensure-deps.sh
@@ -1,38 +1,7 @@
 #!/bin/sh
 # https://www.chezmoi.io/reference/configuration-file/hooks/
 # this script runs every time the source state is read, so the faster is exits, the less you have to wait
-# it ensures that we have git and zsh installed
-
-## git
-if ! command -v "git" > /dev/null; then
-  case "$(uname -s)" in
-    Darwin)
-      xcode-select --install
-      ;;
-    Linux)
-      . /etc/os-release
-      case $ID in
-        debian|ubuntu|mint)
-          export DEBIAN_FRONTEND=noninteractive
-          sudo apt -qq -y install git < /dev/null > /dev/null
-          ;;
-        rocky|fedora|rhel|centos)
-          sudo dnf -y install git 
-          ;;
-        archarm|arch)
-          sudo pacman -S --noconfirm git 
-          ;;
-        *)
-          echo -n "unsupported linux distro, install git manually"
-          ;;
-      esac
-    ;;
-    *)
-      echo "Unknown OS, install git manually"
-      exit 1
-      ;;
-  esac
-fi
+# it ensures that we have zsh installed
 
 ## zsh
 if ! command -v "zsh" > /dev/null; then
