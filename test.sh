32a004f5ba848d1a3cac06ff7b9f00461dc2db1a
Nathanael Liechti <technat@technat.ch>
2025-04-15 21:02:09 +0200

    fix: typo in workflow

--- test.sh
+++ test.sh
@@ -2,8 +2,12 @@
 # very basic test script that ensures that chezmoi has done most of what we expect
 
 set -e # -e: exit on error
+
+## zsh
 zshPath=$(command -v zsh)
-echo $zshPath
+if [ -n "$zshPath" ]; then
+  echo "zsh not found"
+fi
 
 ## git
 # $zshPath -i -c "git config --list"
