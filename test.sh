5ecb484b05428da4fcd0921ed078438932f82bb2
Nathanael Liechti <technat@technat.ch>
2025-04-16 09:46:00 +0200

    feat: extend test.sh script

--- test.sh
+++ test.sh
@@ -1,18 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 # very basic test script that ensures that chezmoi has done most of what we expect
-
 set -e # -e: exit on error
+zshPath=$(command -v zsh)
 
 ## zsh
-zshPath=$(command -v zsh)
-if [ -n "$zshPath" ]; then
-  echo "zsh not found"
-fi
+zsh -i -c "echo hello"
 
 ## git
-# $zshPath -i -c "git config --list"
-
+gitMail=$(zsh -i -c "git config --global user.email")
+if [[ ! $gitMail =~ "technat.ch" ]]; then
+  echo "git config has not been set properly"
+  exit 1
+fi
 
 ## nvim
+zsh -i -c "nvim --version"
 
-## 
+## check some aliases 
+zsh -i -c "command -v ghh"
+zsh -i -c "command -v ssh"
+zsh -i -c "command -v vim"
+
+## mise
+zsh -i -c "mise --version"
