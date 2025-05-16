21407c5116f9b34f416e8dd977e08a3160f85e37
Nathanael Liechti <technat@technat.ch>
2025-05-16 13:53:07 +0200

    fix: set termguicolors in nvim

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -6,11 +6,13 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+
+-- Use TERM GUI colors (allow nvim to look good in tmux on all terminals
+vim.o.termguicolors = true
 
 -- Make line numbers default
 vim.opt.number = true
