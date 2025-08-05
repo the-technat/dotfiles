7841ed24e82163833f86a81f9b3999e6d9d2a793
Nathanael Liechti <technat@technat.ch>
2025-08-05 17:40:59 +0200

    feat: remove deprecated nvim option

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -162,7 +162,7 @@ local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = augroup,
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
