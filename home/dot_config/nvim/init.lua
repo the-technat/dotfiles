0e4a194cf1e6594d96e06fbeb50f6adc8622eefd
Nathanael Liechti <technat@technat.ch>
2025-08-19 09:53:18 +0200

    feat: don't treat as keyword

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -69,7 +69,7 @@ vim.opt.hidden = true                              -- Allow hidden buffers
 vim.opt.errorbells = false                         -- No error bells
 vim.opt.backspace = "indent,eol,start"             -- Better backspace behavior
 vim.opt.autochdir = false                          -- Don't auto change directory
-vim.opt.iskeyword:append("-")                      -- Treat dash as part of word
+--vim.opt.iskeyword:append("-,_")                      -- Treat dash as part of word
 vim.opt.path:append("**")                          -- include subdirectories in search
 vim.opt.selection = "exclusive"                    -- Selection behavior
 vim.opt.mouse = "a"                                -- Enable mouse support
