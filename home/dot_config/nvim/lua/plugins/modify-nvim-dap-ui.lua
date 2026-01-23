8724c3a3bfa174663586727e3e517e52a240f6b3
Nathanael Liechti <technat@technat.ch>
2026-01-23 09:56:26 +0100

    fix(lazyvim): debug ui only needs scopes

--- home/dot_config/nvim/lua/plugins/modify-nvim-dap-ui.lua
+++ home/dot_config/nvim/lua/plugins/modify-nvim-dap-ui.lua
@@ -4,18 +4,10 @@ return {
     layouts = {
       {
         elements = {
-          { id = "scopes", size = 0.75 },
-          { id = "breakpoints", size = 0.25 },
-        },
-        position = "right",
-        size = 40,
-      },
-      {
-        elements = {
-          { id = "repl", size = 1 },
+          { id = "scopes", size = 1 },
         },
         position = "bottom",
-        size = 20,
+        size = 30,
       },
     },
   },
