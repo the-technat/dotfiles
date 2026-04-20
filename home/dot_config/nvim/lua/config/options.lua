80b71f341159c0e9c4d402e834a39ebe4d9b0de6
Nathanael Liechti <technat@technat.ch>
2026-04-20 14:08:49 +0200

    chore: disable unused language providers in nvim

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -1,1 +1,6 @@
 vim.g.vimtex_view_method = "sioyek"
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
