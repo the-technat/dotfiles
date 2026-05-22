e9215a5249d3b405f7004fb0a2ce76e369ddf4da
Nathanael Liechti <technat@technat.ch>
2026-05-22 11:06:35 +0200

    chore: don't explicitly configure neotest with only java adpater

--- home/dot_config/nvim/lua/plugins/add-neotest-java.lua
+++ home/dot_config/nvim/lua/plugins/add-neotest-java.lua
@@ -10,21 +10,4 @@ return {
       "theHamsta/nvim-dap-virtual-text", -- recommended
     },
   },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-java")({
-            -- Optional configuration here
-          }),
-        },
-      })
-    end,
-  },
 }
